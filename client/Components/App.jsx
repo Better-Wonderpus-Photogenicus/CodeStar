b772cd96995a3c9d87628ed0e8808befc4ed6997
Krystal Fung <krystalfung@krystals-mbp.mynetworksettings.com>
2023-03-25 11:38:18 -0400

    containers created front end

---
+++ client/Components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header'
+import CompatabilityContainer from './Containers';
+
+function App() {
+  return (
+    <div>
+      <Header /> 
+      <CompatabilityContainer />
+    </div>
+  )
+}
+
+export default App;
+
+
+
