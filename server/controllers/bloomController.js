895d730758b3b393605e08d03233a1eff16d4bbc
Peter <peter@Peters-Air.attlocal.net>
2023-03-25 11:23:30 -0500

    horoscope api working

---
+++ server/controllers/bloomController.js
@@ -0,0 +1,34 @@
+const dotenv = require('dotenv').config();
+const bloom = {};
+
+bloom.getCompatibility = (req, res, next) => {
+    const { id1, id2 } = req.body;
+    fetch('https://api.bloom.be/api/signtosign', {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + process.env.api_key,
+            "Accept-Encoding": "application/json"
+        },
+        body: JSON.stringify({
+            "sign1_id": id1,
+            "sign2_id": id2,
+        }),
+    })
+    .then(data => data.json())
+    .then(data => {
+        let text =  data.text.text;
+        text = text.slice(0, text.length-4);
+        // const newText = text.replace(/<p>(.*?)<p>/g, '')
+        let pointer = 0;
+        for (let i = 0 ; i < text.length; i++) {
+            if (text[i] === '>') pointer = i;
+        }
+        text = text.slice(pointer+1);
+        res.locals.text = text})
+    .then(()=> next())
+    .catch(err => next(err));
+
+}
+
+module.exports = bloom;
---
+++ server/server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+
+const bloomController = require('./controllers/bloomController');
+const userController = require('./controllers/userController');
+
+const MONGO_URI = 'mongodb+srv://peterfeng96:6nKn8jFLOFpicqD2@cluster0.xpivncd.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(MONGO_URI);
+
+app.use(express.json());
+// app.use(express.urlencoded());
+// app.use(cookieParser());
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/build', express.static(path.join(__dirname, '../build')));
+}
+  
+app.use(express.static(path.join(__dirname, '../client/')));
+
+//ROUTES
+
+app.post('/api', bloomController.getCompatibility, (req, res) => {
+    return res.status(200).json(res.locals.text)
+})
+
+app.post('/signup', userController.createUser, (req, res) => {
+    return res.sendStatus(200);
+})
+
+
+
+
+
+
+
+
+
+// ERROR HANDLING
+
+app.use('*', (req, res) => {
+    return res.status(404);
+});
+  
+app.use((err, req, res, next) => {
+const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+};
+const errorObj = Object.assign({}, defaultErr, err);
+console.log(errorObj.log);
+return res.status(errorObj.status).json(errorObj.message);
+});
+  
+// SET SERVER TO LISTEN 
+
+app.listen(port, () => {
+console.log(`CodeStar listening on port ${port}`)
+});
