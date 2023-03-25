66a255b105e4bb8188ddcb8f8a55c19297f0d763
klfung7 <107321239+klfung7@users.noreply.github.com>
2023-03-25 14:53:36 -0400

    Update FormContainer.jsx

--- client/Components/FormContainer.jsx
+++ client/Components/FormContainer.jsx
@@ -3,12 +3,12 @@ import SignDropdown from "./SignDropdown.jsx";
 
 function FormContainer(props) {
   return (
-    <id="formContainer">
+    <div id="formContainer">
       <SignDropdown id={'userSign'} text={'Your Sign'}/>
       <SignDropdown id={'partnerSign'} text={'Partner\'s Sign'}/>
       <button onClick={props.handleSubmit}>Submit</button>
       <p>Don't know your sign? <a href="https://visualfractions.com/astrology/zodiac-sign/" target="_blank" rel="noopener noreferrer">click here</a></p>
-    </>
+    </div>
   )
 }
 
