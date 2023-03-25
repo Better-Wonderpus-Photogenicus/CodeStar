4c5a949daf166fd629aba500f840d89ff9d1bb00
Hazel Bolivar <hazelbolivar@Hazels-MacBook-Pro.local>
2023-03-25 14:33:16 -0400

    builds FormContainer

--- client/Components/Containers.jsx
+++ client/Components/Containers.jsx
@@ -1,12 +1,34 @@
 import React from "react";
-import FormContainer from "./FormContainer";
-import ResultsContainer from "./ResultsContainer";
+import FormContainer from "./FormContainer.jsx";
+import ResultsContainer from "./ResultsContainer.jsx";
 
 function CompatabilityContainer() {
+  const [text, setText] = React.useState('')
+
+  function handleSubmit(){
+    const id1 = document.getElementById('userSign').value
+    const id2 = document.getElementById('partnerSign').value
+    const body = {
+      id1,
+      id2,
+    }
+    fetch('/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+    .then(response => response.json)
+    .then(text => setText(text))
+    .catch(err => console.log(err))
+    console.log('User:', id1, 'Partner:', id2)
+  }
+
   return (
     <div>
-      <FormContainer />
-      <ResultsContainer />
+      <FormContainer handleSubmit={handleSubmit}/>
+      <ResultsContainer text={text}/>
     </div>
   )
 }
