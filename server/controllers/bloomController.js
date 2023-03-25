b4b6bbfa85404fd65db56e232776a54b78c44d71
Peter <peter@Peters-Air.attlocal.net>
2023-03-25 15:31:58 -0500

    half finished cache

--- server/controllers/bloomController.js
+++ server/controllers/bloomController.js
@@ -1,5 +1,19 @@
 const dotenv = require('dotenv').config();
+const cache = require('../models/cacheModel');
 const bloomController = {};
+
+bloomController.checkCache = async (req, res, next) => {
+  const { id1, id2 } = req.body;
+  const key = [id1, id2].sort().join();
+
+  const found = await cache.findOne({ key });
+  // look up what findOne returns
+  // if it returns something, then pass that in to the user
+  // if not, move on to getcompatibility from API
+  // also write a storecompatibility function for when we get one :)
+  // also error handling
+
+}
 
 bloomController.getCompatibility = (req, res, next) => {
     const { id1, id2 } = req.body;
